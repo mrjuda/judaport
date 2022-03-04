ae28b9b0f4d4647033ef38de66232c9bce2bca4e
judah <87juda@gmail.com>
2022-03-04 14:46:10 -0300

    Add basic data to the cards on js

---
+++ scripts/dynamicCards.js
@@ -0,0 +1,117 @@
+// dynamicCards.js
+
+const cardData = [
+  {
+    // CARD 1 //
+    cardId: "card1",
+    popupId: "popup1",
+    popupX: "close-popup-1",
+    imgSrc: "images/pic1.png",
+    imgAlt: "Tonic sneakpeak",
+    project: "Tonic",
+    company: "CANOPY",
+    position: "Back End Dev",
+    year: "2014",
+    cardP: `
+    A daily selection of privately personalized 
+    reads; no accounts or sign-ups required
+    `,
+    popupP: `
+    Lorem Ipsum is simply dummy text of the printing 
+    and typesetting industry. Lorem Ipsum has been the 
+    industry's standard dummy text ever since the 1500s, 
+    when an unknown printer took a galley of type and scrambled 
+    it to make a type specimen book. It has survived not 
+    only five centuries, but also the leap into electronic 
+    typesetting, remaining essent
+    `,
+    cardCtaBtn: "cta-card1",
+    demoSrc: "#",
+    githubSrc: "#",
+  },
+  {
+    // CARD 2 //
+    cardId: "card2",
+    popupId: "popup2",
+    popupX: "close-popup-2",
+    imgSrc: "images/pic2.png",
+    imgAlt: "Multi-Post Stories sneakpeak",
+    project: "Multi-Post Stories",
+    company: "Facebook",
+    position: "Full Stack Dev",
+    year: "2016",
+    cardP: `
+    A daily selection of privately personalized 
+    reads; no accounts or sign-ups required
+    `,
+    popupP: `
+    Lorem Ipsum is simply dummy text of the printing 
+    and typesetting industry. Lorem Ipsum has been the 
+    industry's standard dummy text ever since the 1500s, 
+    when an unknown printer took a galley of type and scrambled 
+    it to make a type specimen book. It has survived not 
+    only five centuries, but also the leap into electronic 
+    typesetting, remaining essent
+    `,
+    cardCtaBtn: "cta-card2",
+    demoSrc: "#",
+    githubSrc: "#",
+  },
+  {
+    // CARD 3 //
+    cardId: "card3",
+    popupId: "popup3",
+    popupX: "close-popup-3",
+    imgSrc: "images/pic3.png",
+    imgAlt: "Facebook 360 Sneakpeak",
+    project: "Facebook 360",
+    company: "Facebook",
+    position: "Full Stack Dev",
+    year: "2017",
+    cardP: `
+    A daily selection of privately personalized 
+    reads; no accounts or sign-ups required
+    `,
+    popupP: `
+    Lorem Ipsum is simply dummy text of the printing 
+    and typesetting industry. Lorem Ipsum has been the 
+    industry's standard dummy text ever since the 1500s, 
+    when an unknown printer took a galley of type and scrambled 
+    it to make a type specimen book. It has survived not 
+    only five centuries, but also the leap into electronic 
+    typesetting, remaining essent
+    `,
+    cardCtaBtn: "cta-card3",
+    demoSrc: "#",
+    githubSrc: "#",
+  },
+  {
+    // CARD 4 //
+    cardId: "card4",
+    popupId: "popup4",
+    popupX: "close-popup-4",
+    imgSrc: "images/pic4.png",
+    imgAlt: "Uber Navigation Sneakpeak",
+    project: "Uber Navigation",
+    company: "Uber",
+    position: "Lead Developer",
+    year: "2019",
+    cardP: `
+    A daily selection of privately personalized 
+    reads; no accounts or sign-ups required
+    `,
+    popupP: `
+    Lorem Ipsum is simply dummy text of the printing 
+    and typesetting industry. Lorem Ipsum has been the 
+    industry's standard dummy text ever since the 1500s, 
+    when an unknown printer took a galley of type and scrambled 
+    it to make a type specimen book. It has survived not 
+    only five centuries, but also the leap into electronic 
+    typesetting, remaining essent
+    `,
+    cardCtaBtn: "cta-card4",
+    demoSrc: "#",
+    githubSrc: "#",
+  },
+];
+
