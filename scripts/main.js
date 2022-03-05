1207c85343507c21af4069f27c646151624aca06
judah <87juda@gmail.com>
2022-03-05 19:33:29 -0300

    First email validation test

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,14 @@ function loadJS(url) {
 loadJS('scripts/mobileMenu.js');
 loadJS('scripts/dynamicCards.js');
 loadJS('scripts/popup.js');
+
+const email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an e-mail address!");
+    email.reportValidity();
+  } else {
+    email.setCustomValidity("");
+  }
+});
